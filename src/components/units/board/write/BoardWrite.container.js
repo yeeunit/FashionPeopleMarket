3933d2c78a317f0c03a14b37c049325af817e1d7
yeeunit <yeeunit22@gmail.com>
2022-11-07 23:57:25 +0900

    11/7

--- src/components/units/board/write/BoardWrite.container.js
+++ src/components/units/board/write/BoardWrite.container.js
@@ -1,28 +1,112 @@
 import { useMutation } from "@apollo/client";
 import { Modal } from "antd";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { UPLOAD_FILE } from "../../../commons/uploads/01/Upload.queries";
 import BoardWriteUI from "./BoardWrite.presenter";
 import { CREATE_BOARD } from "./BoardWrite.queries";
 
-export default function BoardWrite() {
-  const fileRef = useRef(null);
+export default function BoardWrite(props) {
+  const initialInputs = { writer: "", password: "", title: "", contents: "" };
+
+  const [inputs, setInputs] = useState(initialInputs);
+  const [inputsError, setInputsError] = useState(initialInputs);
 
   const router = useRouter();
+  const [isActive, setIsActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const fileRef = useRef(null);
+
+  // const [writer, setWriter] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [contents, setContents] = useState("");
+
+  const [youtubeUrl, setYoutubeUrl] = useState("");
+  const [zipcode, setZipcode] = useState("");
+  const [address, setAddress] = useState("");
+  const [addressDetail, setAddressDetail] = useState("");
+
+  const [imageUrl, setImageUrl] = useState(["", "", ""]);
+
   const [createBoard] = useMutation(CREATE_BOARD);
 
-  const [writer, setWriter] = useState("");
-  const [password, setPassword] = useState("");
-  const [title, setTitle] = useState("");
-  const [contents, setContents] = useState("");
-  // const [youtubeUrl, setYoutubeUrl] = useState("")
-  const [imageUrl, setImageUrl] = useState();
   const [uploadFile] = useMutation(UPLOAD_FILE);
 
-  const [emailError, setEmailError] = useState("");
+  // const [emailError, setEmailError] = useState("");
+
+  const onChangeInputs = (event) => {
+    const _inputs = {
+      ...inputs,
+      [event.target.id]: event.target.value,
+    };
+    setInputs(_inputs);
+    if (event.target.value !== "") {
+      setInputsError({
+        ...inputsError,
+        [event.target.id]: "",
+      });
+    }
+
+    if (Object.values(_inputs).every((el) => el)) {
+      setInputs(true);
+    } else {
+      setIsActive(false);
+    }
+  };
+
+  const onChangeYoutubeYul = (event) => {
+    setYoutubeUrl(event.target.value);
+  };
+
+  const onChangeAddressDetail = (event) => {
+    setAddressDetail(event.target.value);
+  };
+
+  const onClickAddressSearch = () => {
+    setIsOpen(true);
+  };
+
+  const onClickImage = () => {
+    fileRef.current?.click();
+  };
+
+  const onChangeFileUrls = (fileUrl, index) => {
+    const newFileUrls = [...fileUrls];
+    newFileUrls[index] = fileUrl;
+    setFileUrls(newFileUrls);
+  };
+
+  useEffect(() => {
+    if (props.data?.fetchBoard.images?.length) {
+      setFileUrls([...props.data?.fetchBoard.images]);
+    }
+  }, [props.data]);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible((prev) => !prev);
+  };
+
+  const onCompletePostcode = (data) => {
+    console.log(data);
+    setIsModalVisible(false);
+  };
+
+  const onChangeAddress = (event) => {
+    setAddress(event.target.value);
+  };
 
   const onClickRegister = async () => {
+    const errors = {
+      writer: "작성자를 입력해주세요",
+      password: "비밀번호를 입력해주세요",
+      title: "제목을 입력해주세요",
+      contents: "내용을 입력해주세요",
+    };
+
     try {
       const result = await createBoard({
         variables: {
@@ -43,54 +127,6 @@ export default function BoardWrite() {
       console.log(error.message);
       Modal.error({ content: "등록 실패!!" });
     }
-  };
-
-  const onChangeWriter = (event) => {
-    setWriter(event.target.value);
-  };
-
-  const onChangePassword = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const onChangeTitle = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const onChangeContents = (event) => {
-    setContents(event.target.value);
-  };
-
-  const onChangeFile = async (event) => {
-    const file = event.target.files?.[0];
-    console.log(file);
-
-    try {
-      const result = await uploadFile({ variables: { file } });
-      setImageUrl(result.data.uploadFile.url);
-    } catch (error) {
-      Modal.error({ content: "에러발생" });
-    }
-  };
-
-  const onClickImage = () => {
-    fileRef.current?.click();
-  };
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [address, setAddress] = useState("");
-
-  const showModal = () => {
-    setIsModalVisible((prev) => !prev);
-  };
-
-  const onCompletePostcode = (data) => {
-    console.log(data);
-    setIsModalVisible(false);
-  };
-
-  const onChangeAddress = (event) => {
-    setAddress(event.target.value);
   };
 
   return (
--- src/components/units/board/write/BoardWrite.queries.js
+++ src/components/units/board/write/BoardWrite.queries.js
@@ -13,3 +13,32 @@ export const CREATE_BOARD = gql`
     }
   }
 `;
+
+export const UPDATE_BOARD = gql`
+  mutation updateBoard(
+    $updateBoardInput: UpdateBoardInput!
+    $password: String
+    $boardId: ID!
+  ) {
+    updateBoard(
+      updateBoardInput: $updateBoardInput
+      password: $password
+      boardId: $boardId
+    ) {
+      _id
+      writer
+      title
+      contents
+      youtubeUrl
+      likeCount
+      dislikeCount
+      images
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      createdAt
+    }
+  }
+`;
