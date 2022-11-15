02173e3e6077898f89bfb54996ed434502fcd52e
yeeunit <yeeunit22@gmail.com>
2022-11-15 23:56:13 +0900

    11/15

--- src/components/units/mypage/Mypage.presenter.js
+++ src/components/units/mypage/Mypage.presenter.js
@@ -6,16 +6,25 @@ export default function MypageUI(props) {
   return (
     <>
       <A.Wrapper>
-        <A.Title>My page</A.Title>
-
-        <A.TopWrapper>
+        <A.LeftWrap>
+          <A.Profile></A.Profile>
+          <A.Name>Lady GaGa</A.Name>
+          <A.Tabs>회원정보 수정</A.Tabs>
+          <A.Tabs>내가 쓴 글</A.Tabs>
+          <A.Tabs>찜 목록</A.Tabs>
+          <A.Tabs>내 포인트</A.Tabs>
+        </A.LeftWrap>
+        <A.RightWrap>
+          <A.Title>My page</A.Title>
+        </A.RightWrap>
+        {/* <A.TopWrapper>
           <A.box1>
             <div>{props.data?.props.fetchUserLoggedIn.name}님 환영합니다!</div>;
           </A.box1>
           <A.box2></A.box2>
           <A.box3></A.box3>
         </A.TopWrapper>
-        <A.bar></A.bar>
+        <A.bar></A.bar> */}
       </A.Wrapper>
     </>
   );
--- src/components/units/mypage/Mypage.styles.js
+++ src/components/units/mypage/Mypage.styles.js
@@ -2,11 +2,48 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
   align-items: center;
   margin: 50px auto;
 `;
 export const Title = styled.h1``;
+
+export const LeftWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 22rem;
+  height: 70rem;
+  border-right: 1px solid #555;
+`;
+
+export const Profile = styled.div`
+  width: 10rem;
+  height: 10rem;
+  margin-top: 5rem;
+  background-color: #eee;
+  border-radius: 999px;
+`;
+
+export const Name = styled.div`
+  margin-top: 3rem;
+  font-size: 1.5rem;
+`;
+
+export const Tabs = styled.div`
+  margin-top: 1.5rem;
+  font-size: 1.1rem;
+  cursor: pointer;
+  :hover {
+    border-bottom: 1px solid #555;
+  }
+`;
+
+export const RightWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const TopWrapper = styled.div`
   display: flex;
