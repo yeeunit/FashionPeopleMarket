9d93a624bdf43eed028143e8aa64053c2a8a87fb
yeeunit <yeeunit22@gmail.com>
2023-01-18 23:46:01 +0900

    1/18

--- src/commons/styles/commonStyles.js
+++ src/commons/styles/commonStyles.js
@@ -3,6 +3,12 @@ export const DarkBlackColor = "#222";
 export const LightGrayColor = "#eee";
 export const BlueColor = "#0070f3";
 
+export const Sky = "#7ABFBF";
+export const Green = "#308C7B";
+export const Orange = "#F24535";
+export const Red = "#BF372A";
+export const Burgundy = "#731717";
+
 export const RainbowColor = [
   "red",
   "orange",
